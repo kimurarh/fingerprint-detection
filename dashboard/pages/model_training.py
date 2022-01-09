6c7eb8e24a0a202b191566a4e8958c0d3d68c761
Rafael Kimura <kimurarh@gmail.com>
2022-01-08 21:49:28 -0300

    add: model architecture and training description

--- dashboard/pages/model_training.py
+++ dashboard/pages/model_training.py
@@ -4,4 +4,23 @@ __all__ = ["model_training"]
 
 
 def model_training():
-    st.write("## Model Training")
+    st.write("## Model")
+
+    # Assets' paths
+    model_architecture = "../assets/model_architecture.png"
+    training_validation_acc = "../assets/training_validation_accuracy.png"
+    training_validation_loss = "../assets/training_validation_loss.png"
+
+    st.write("### Model Architecture")
+    st.image(model_architecture)
+    st.write("")
+    st.write("### Data Split")
+    st.write("""
+             >- `39,416` samples for training (altered fingerprints)
+             >- `9,854` samples for validation (altered fingerprints)
+             >- `6,000` samples for testing (real fingerprints)
+             """)
+    st.write("")
+    st.write("### Model Training Process")
+    st.image(training_validation_acc)
+    st.image(training_validation_loss)
